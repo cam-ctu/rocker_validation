6464b0d09c12c5d08137ae2be7696f519cb21802
shug0131 <sjb277@medschl.cam.ac.uk>
2024-11-08 13:38:39 +0000

    further work to do individual package test.  No vignettes insider Rmarkdown at present.

--- extra_package_test.R
+++ extra_package_test.R
@@ -2,17 +2,18 @@
 pkg <- "ggalluvial"
 # check if there is a test folder in the installed library.
 
-dl <- download.packages(pkg, destdir="~/pack",type="source",
+dl <- download.packages(pkg, destdir="package",type="source",
                         method="wget")# wget was what solved this.. was getting binary versions
 
 #unzip the tar.gz file
 # 
 
+unlink(paste0("package/",pkg))
+untar(dl[,2], exdir = "package", restore_times=FALSE)
 
-file <- list.files("~/pack", full.names = TRUE)
-untar(file, exdir = normalizePath("~/pack"))
 # modify the testInstalledpackages to use the /test fold from within pack.
-test_pkg("ggalluvial", outDir="~/pack", test_path=normalizePath("~/pack/ggalluvial/tests"))
+test_pkg("ggalluvial", outDir="package/ggalluvial", test_path="tests")
+# the test_path is relative to the outDir... c
 # the above runs the test, the below does not as /test directory was not installed by default.
-tools::testInstalledPackage("ggalluvial", outDir="~/pack")
+#tools::testInstalledPackage("ggalluvial", outDir="~/pack")
 
